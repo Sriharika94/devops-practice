a9c22137b029f60e8325bfc444a06e48ab09f947
Sriharika <asurisriharika94@gmail.com>
2024-08-26 23:37:28 +0530

    variables

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2"
+echo "$Person2: Hi $Person1"
+echo "$Person1: How are you"
+echo "$Person2: Iam fine , How about you"
+echo "$Person1: Iam fine thank you"

acefc7d5914bfc1fbbe3f37e0859727495a29c2c
Sriharika <asurisriharika94@gmail.com>
2024-08-26 23:48:32 +0530

    variables

---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Fruits=("Apple" "Kiwi" "orange")
+
+echo "first fruit is $Fruits[0]"
+echo "second fruit is $Fruits[1]"
+echo "third fruit is $Fruits[2]"
+echo "fruits of all $Fruits[@]"

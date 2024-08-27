838574ec3edf8c389c68d6d8fec093b0fb279cf6
Sriharika <asurisriharika94@gmail.com>
2024-08-27 15:43:31 +0530

    variables

--- installation.sh
+++ installation.sh
@@ -8,4 +8,12 @@ then
     echo "please run the script with root privilages"
     exit 1
 fi
-dnf install git -y
+dnf list installed git
+
+if [$? -ne 0 ]
+then 
+    echo "git is not installed ,installing now"
+    dnf install git -y
+else
+    echo "git is already installed"
+fi    

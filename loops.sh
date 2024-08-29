470963c9aeae278aaf205b0758d345ca42b669cb
Sriharika <asurisriharika94@gmail.com>
2024-08-29 20:39:13 +0530

    variables

--- loops.sh
+++ loops.sh
@@ -31,7 +31,7 @@ fi
 for package in $@
 do
     dnf list installed $package
-    if[ $? -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "$package is not installed installing it"
         dnf installing $package -y

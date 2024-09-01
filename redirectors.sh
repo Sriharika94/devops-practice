a1469eb6e41dba9bb89f895eeabb680d284586f7
Sriharika <asurisriharika94@gmail.com>
2024-09-01 14:44:59 +0530

    variables

--- redirectors.sh
+++ redirectors.sh
@@ -44,7 +44,7 @@ CHECK_ROOT
 
 for package in $@
 do
-    dnf list installed $package | tee -a $LOG_FILE
+    dnf list installed $package  &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
         echo "$package is not installed installing it" | tee -a $LOG_FILE

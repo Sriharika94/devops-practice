ccd7fe13cf1951438f2dae7dce830655811f85de
Sriharika <asurisriharika94@gmail.com>
2024-09-02 22:58:13 +0530

    variables

--- deletelogfiles.sh
+++ deletelogfiles.sh
@@ -9,3 +9,13 @@ else
     echo "$SOURCE_DIR doesnt exists"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files =$FILES"
+
+while IFS=read -r file #IFS , internal field separator,empty will ignore ,-r means not to ignore special characters like /
+do
+    echo "deleting file "$file"
+    rm -rf $file
+done<<<
+

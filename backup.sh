121e6a3fd51f6463023b9e31f11b21bb5b7a46d3
Sriharika <asurisriharika94@gmail.com>
2024-09-03 21:31:52 +0530

    variables

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+ SOURCE_DIR=$1
+ DEST_DIR=$2
+ DAYS=$(3: -14)
+
+
+USAGE(){
+    echo -e "USAGE:: sh backup.sh <source> <destination> <days>"
+
+}
+ #check source and desination are provided
+
+ if [ $# -lt 2 ]
+ then
+    USAGE 
+fi
